1a29acdf5773784e0e8964a3ce06dab1ebd9e1fb
James Clarke <j.kingston.c@gmail.com>
2024-01-03 18:49:30 +0000

    build

--- src/codegen.rs
+++ src/codegen.rs
@@ -1,4 +1,6 @@
-use log::{debug, info, warn};
+use log::{debug, error, info, warn};
+use std::fs;
+use std::io::Write;
 use std::time::Instant;
 
 pub struct CodeGenerator {}
@@ -7,6 +9,31 @@ impl CodeGenerator {
     pub fn generate(&self) {
         debug!("beginning code gen.");
         let now = Instant::now();
+
+        match fs::create_dir_all("./build") {
+            Ok(_) => {}
+            Err(err) => {
+                error!("failed to create build directory /build {}.", err);
+                panic!("failed to create build directory /build {}.", err);
+            }
+        }
+
+        let mut file = match fs::File::create("./build/build.exe") {
+            Err(err) => {
+                error!("failed to create executable {}.", err);
+                panic!("failed to create executable {}.", err);
+            }
+            Ok(file) => file,
+        };
+
+        match file.write_all("hello, world".as_bytes()) {
+            Err(err) => {
+                error!("failed to write to executable {}.", err);
+                panic!("failed to write to executable {}.", err);
+            }
+            Ok(file) => {}
+        }
+
         for i in 0..100000000 {}
         let elapsed = now.elapsed();
         debug!(
